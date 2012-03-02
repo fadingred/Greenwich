a78878cb80c58df04c2b96701418e60a6a9d9915
Whitney <wbyoung@fadingred.com>
2012-03-02 15:13:26 -0600

    Added a migration script.
    This allows easier migration to Greenwich from multiple xib files.

--- Framework/Scripts/lib/scriptlib/i18n/xib.py
+++ Framework/Scripts/lib/scriptlib/i18n/xib.py
@@ -23,13 +23,15 @@ import sys
 import os
 
 class Xib(object):
-  def __init__(self, name, config):
+  def __init__(self, name, config, path=None):
     self.name = clean_name(name)
     self.config = config
+    self._path = path
     self._mtime = None
     self._gtime = None
   
   def path(self):
+    if self._path: return self._path
     resources = self.config.resources
     path = os.path.join(resources, '%s.xib' % self.name)
     return path
@@ -57,7 +59,7 @@ class Xib(object):
     if not os.path.exists(directory):
       os.mkdir(directory)
   
-  def generate(self, strings):
+  def generate(self, strings, normalize=True):
     """
     Generate strings file in an expected format and update generation time
     """
@@ -73,7 +75,7 @@ class Xib(object):
     result = process.wait()
     if result != 0:
       sys.stderr.write('warning: error generating strings, see prior output for information\n');
-    strings.normalize()
+    if normalize: strings.normalize()
     generation = self._generation()
     self._ensure_dir_for_file(generation)
     os.system('touch "%s"' % (generation,))
